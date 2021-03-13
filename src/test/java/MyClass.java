9692cb1fe26e9449137ca27dab8767d63c96cdf2
Łukasz Krause <lukasz.krause@onet.eu>
2021-03-13 11:01:06 +0100

    Add test

--- src/test/java/MyClass.java
+++ src/test/java/MyClass.java
@@ -1,15 +1,28 @@
-/**
- * Created by Lukasz Krause
- * Date: 13.03.2021
- */
+import org.junit.Test;
 
 public class MyClass {
-    String s = "myString";
-    int total = 100;
-    double d2 = 50.10;
 
+    String s = "myString";
+    String locator = ".class";
+    String s2 = "string1" + "_string2";
+    String s3 = "string4" + 10;
+    String s4 = 10+5+"string";
+    String s5 = "string"+10+5;
+
+
+    int total = 100;
+    double d = 100.5;
+    Double d2 = 50.0;
+
+  @Test
     public void test(){
-        String s1 = "smth";
+        String s1= "smth";
+      System.out.println(s4);
+      System.out.println(s5);
     }
 
+    public void test2(){
+       // s1 = "hi";
+        total = 5;
+    }
 }
