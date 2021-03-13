dfe365197b5e4fb35f862ae300098a5ff87122ee
Łukasz Krause <lukasz.krause@onet.eu>
2021-03-13 10:44:00 +0100

    Add

---
+++ src/test/java/MyClass.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Lukasz Krause
+ * Date: 13.03.2021
+ */
+
+public class MyClass {
+    String s = "myString";
+    int total = 100;
+
+    public void test(){
+        String s1 = "smth";
+    }
+}
