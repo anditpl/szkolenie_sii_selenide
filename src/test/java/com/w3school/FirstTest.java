6783eba7c1f1d86bbbbabf4d41fa56b5ff0a6ac3
Łukasz Krause <lukasz.krause@onet.eu>
2021-03-13 16:30:42 +0100

    Add test Selenide

--- src/test/java/com/w3school/FirstTest.java
+++ src/test/java/com/w3school/FirstTest.java
@@ -3,6 +3,7 @@ package com.w3school;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
+import org.junit.Assert;
 import org.junit.Test;
 
 import static com.codeborne.selenide.Condition.attribute;
@@ -16,12 +17,14 @@ public class FirstTest {
     public void clickAroundW3School(){
 
         open("https://www.w3schools.com/cssref/css_selectors.asp");
+        System.out.println("Test " + WebDriverRunner.url());
 
         $("#accept-choices").click();
 //        $(screenshot("test"));
 
 
-        $("#main h2").shouldHave(Condition.exactText("CSS Selectors"));
+//        $("#main h2").shouldHave(Condition.exactText("CSS Selectors"));
+        assertH2ContainsText("CSS Selectors");
         $("title").shouldHave(attribute("text", "CSS Selectors Reference"));
 
         $$("#leftmenuinnerinner>a").find(exactText("CSS Functions")).click();
@@ -30,5 +33,10 @@ public class FirstTest {
         $("[title*='JavaScript']").click();
         $("#main>h1").shouldHave(Condition.text("JavaScript"));
         assertEquals("https://www.w3schools.com/js/default.asp", WebDriverRunner.url());
+    }
+
+    private void assertH2ContainsText(String menuName){
+        $("#main h2").shouldHave(Condition.exactText(menuName));
+
     }
 }
